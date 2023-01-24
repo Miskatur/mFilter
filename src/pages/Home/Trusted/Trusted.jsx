676c7c12f3d9a24cd741aaf0c54922262823d2ce
sajid9962 <clashking1545@gmail.com>
2023-01-24 19:14:40 +0600

    slider fixed

--- src/pages/Home/Trusted/Trusted.jsx
+++ src/pages/Home/Trusted/Trusted.jsx
@@ -17,64 +17,59 @@ const Trusted = () => {
 	return (
 		<div>
 			<div className="my-40">
-				
-
 				<Swiper
-					slidesPerView={3}
-					spaceBetween={50}
+					slidesPerView={6}
+					spaceBetween={30}
 					slidesPerGroup={2}
-					
 					loop={true}
+					loopFillGroupWithBlank={true}
 					navigation={true}
-					// loopFillGroupWithBlank={true}
 					autoplay={{
 						delay: 2000,
 						disableOnInteraction: false,
 					}}
-					modules={[Autoplay, Navigation,Grid]}
+					modules={[Autoplay, Navigation, Grid]}
 					className="mySwiper lg:h-full">
-					<div className="">
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={flipcart} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={icicBank} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={amazon} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={macdonald} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={pidilite} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={uniliver} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={hdfc_bank} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-						<SwiperSlide className="">
-							<div className="p-5 border shadow-xl rounded-2xl mb-10">
-								<img src={pidilite} alt="" className="w-[235px] h-[100px]" />
-							</div>
-						</SwiperSlide>
-					</div>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={flipcart} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={icicBank} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={amazon} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={macdonald} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={pidilite} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={uniliver} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={hdfc_bank} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
+					<SwiperSlide className="">
+						<div className="p-5 border shadow-xl rounded-2xl mb-5">
+							<img src={pidilite} alt="" className="w-[235px] h-[100px]" />
+						</div>
+					</SwiperSlide>
 				</Swiper>
 			</div>
 		</div>
