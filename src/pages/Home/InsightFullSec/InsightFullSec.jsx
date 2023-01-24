c9ea5931a038559ce6432b1f6c89283201171489
sajid9962 <clashking1545@gmail.com>
2023-01-24 22:28:44 +0600

    insightful section slider fixed

--- src/pages/Home/InsightFullSec/InsightFullSec.jsx
+++ src/pages/Home/InsightFullSec/InsightFullSec.jsx
@@ -37,7 +37,7 @@ const InsightFullSec = () => {
 						// 		slidesPerView: 6,
 						// 	},
 						// }}
-						slidesPerView={6}
+						slidesPerView={"auto"}
 						spaceBetween={30}
 						slidesPerGroup={2}
 						loop={true}
